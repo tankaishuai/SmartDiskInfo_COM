77dcc9606a098d0b0e4422285bad40eca2bf2d42
tankaishuai <tankaishuai@126.com>
2020-07-05 16:57:13 +0800

    update to v56

--- test_SmartDiskInfo_COM.lua
+++ test_SmartDiskInfo_COM.lua
@@ -1,9 +1,10 @@
-﻿win32exts.load_sym("*", "*")
+win32exts.load_sym("*", "*")
+win32exts.co_init(true)
 
 SmartDevice = win32exts.create_object("{4cc61604-07eb-4e25-b336-fc1b93e3fb1a}")
 --"{1a3aa9dd-5a95-42dd-8a22-0c95af915d3f}") SmartResult
 
-sr = SmartDevice.invoke("get_drive_info", "C:")
+sr = SmartDevice.invoke("get_drive_info", "D:")
 sr.add_ref()
 dispId = SmartDevice.find_sym("get_drive_info")
 
@@ -13,6 +14,7 @@ si.add_ref()
 dispId = sr.find_sym("root_item")
 sn = si.invoke("item_by_key", "serial_number")
 sn.add_ref()
+win32exts.println(sn)
 strtext = sn.invoke("to_string")
 
 win32exts.MessageBoxW(nil, {strtext}, {"serial_number of C:"}, 0)
